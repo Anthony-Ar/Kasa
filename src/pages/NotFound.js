6eef0a0b4bf674c9d9b3467fa8521cd7c836aad2
Anthony <anthony.arrighi@orange.fr>
2023-04-18 12:57:55 +0200

    fix: error class/classname

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,8 +4,8 @@ import "../assets/pages/not-found.scss";
 export default function NotFound() {
     return ( 
         <section id="not-found">
-            <p class="title">404</p>
-            <p class="subtitle">Oups! La page que vous demandez n'existe pas.</p>
+            <p className="title">404</p>
+            <p className="subtitle">Oups! La page que vous demandez n'existe pas.</p>
             <Link to="/">Retourner sur la page d'accueil</Link>
         </section>
     )
